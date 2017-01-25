c985dcb265cfccdd567ed470d764622075be6352
TaHuC <TaHuC@hotmail.com>
2017-01-25 22:14:55 +0200

    Refactor code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int inputNum = int.Parse(Console.ReadLine());
+			int sum = 0; int specialNum = 0; bool special = false;
+			for (int num = 1; num <= inputNum; num++)
+			{
+				specialNum = num;
+				while (num > 0)
+				{
+					sum += num % 10;
+					num = num / 10;
+				}
+				special = (sum == 5) || (sum == 7) || (sum == 11);
+				Console.WriteLine($"{specialNum} -> {special}");
+				sum = 0;
+				num = specialNum;
+			}
+		}
+	}
+}
